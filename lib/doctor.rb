07d8afbd5d8ddf6ccd0aa0fc0e6775aead092545
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-30 21:16:39 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+  
+  def appointments
+    Appointment.all.select {|key| key.doctors == self} 
+  end
+  
+  def patients
+    appointments.map(&:patient)
+  end
+  
+end
+
---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+ attr_accessor :name
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select {|key| key.patients == self}
+  end
+  
+  def doctors
+    appointments.map(&:doctor)
+  end
+  
+  
+end
+
+
+  
