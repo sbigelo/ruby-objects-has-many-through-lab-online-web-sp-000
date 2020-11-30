10f7f49319e207da4840d8645d1e14e87cf4a63f
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-30 21:28:53 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+# class Song
+#   attr_accessor :name
+#   attr_reader :artist, :genre
+  
+#   @@all = []
+  
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@all << self
+#   end
+  
+#   def self.all
+#     @all
+#   end
+  
+  
+# end
+
+  
+class Song
+  attr_reader :artist, :genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
